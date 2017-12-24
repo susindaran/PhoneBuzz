4c6775259bea040a32eb0ac16c71c39bc141b346
Susindaran Elangovan <susindaran94@gmail.com>
2017-12-23 19:44:43 -0800

    Basic fizzbuzz voice response

--- twiml-quickstart.rb
+++ twiml-quickstart.rb
@@ -7,3 +7,38 @@ get '/hello-monkey' do
     r.say 'Hello Monkey'
   end.to_s
 end
+
+get '/fizzbuzz' do
+  Twilio::TwiML::VoiceResponse.new do |response|
+
+    response.gather(input: 'dtmf', action: '/start-fizzbuzz', method: 'GET') do |gather|
+      gather.say('Please enter a number')
+    end
+
+    response.say('You did not enter any number')
+
+  end.to_s
+end
+
+get '/start-fizzbuzz' do
+  number = params['Digits'].to_i
+  Twilio::TwiML::VoiceResponse.new do |response|
+    for i in 1..number
+      response.say convert_number(i)
+    end
+  end.to_s
+end
+
+def convert_number( number )
+  if number % 3 == 0 and number % 5 == 0
+    return 'Fizz Buzz'
+  end
+
+  if number % 3 == 0
+    'Fizz'
+  elsif number % 5 == 0
+    'Buzz'
+  else
+    number.to_s
+  end
+end
