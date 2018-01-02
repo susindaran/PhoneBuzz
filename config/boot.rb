7d281f7229e9f1ae3b79a03bb6d1aee0c46ac6e1
Susindaran Elangovan <susindaran94@gmail.com>
2018-01-02 13:41:39 -0800

    Changing logger to puts in shutdown hook

--- config/boot.rb
+++ config/boot.rb
@@ -3,7 +3,7 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 
 END {
-  Rails.logger.info 'Shutting down scheduler and killing existing jobs'
+  puts 'Shutting down scheduler and killing existing jobs'
   Rufus::Scheduler.singleton.shutdown(kill: true)
-  Rails.logger.info 'Scheduler shutdown!'
+  puts 'Scheduler shutdown!'
 }
